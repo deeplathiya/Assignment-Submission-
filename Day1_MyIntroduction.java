6573001c6e58b222e30b2c89f9f4b81c44e99c1c
deeplathiya <91689859+deeplathiya@users.noreply.github.com>
2021-10-02 09:46:26 +0530

    Day-1 Assignment is Submitted

---
+++ Day1_MyIntroduction.java
@@ -0,0 +1,15 @@
+package com.company;
+public class Day1_MyIntroduction
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello! Everyone");
+        System.out.println("\t-> My name is Deep Lathiya.I am from Surat,Gujarat.");
+        System.out.println("\t-> I am currently studying at \'IIT KHARAGPUR\' in : Aerospace Engineering.");
+        System.out.println("\t-> My hobbies are: \"Travelling Historical places, Reading novels and Work on computers.\"");
+        System.out.println("\t-> I like to Learn new things in coding parts.");
+        System.out.println("\t-> Apart from it I also have very keen interest in \"Mathematics and machine learning department\"");
+        System.out.println("\t-> My ultimate goal is to take internship in big companies like: \'Google\' , \'Amazon\' etc..");
+        System.out.println("\t   Thank you.");
+    }
+}
