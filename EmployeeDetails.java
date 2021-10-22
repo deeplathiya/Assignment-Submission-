0b675a00662ab01a174aaa613e1ffa8a0c9a87c6
deeplathiya <91689859+deeplathiya@users.noreply.github.com>
2021-10-22 13:00:52 +0530

    Day 2 + Day 3 Assignment submitted

---
+++ EmployeeDetails.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Employee123
+{
+    private int eid;
+    private String name;
+    private float salary;
+    public void inputDetails()
+    {
+        Scanner sc=new Scanner(System.in);
+        Scanner sc1=new Scanner(System.in);
+        System.out.print("Enter Employee ID    :");
+        eid=sc.nextInt();
+        System.out.print("Enter Employee Name  :");
+        name=sc1.nextLine();
+        System.out.print("Enter Employee Salary:");
+        salary=sc.nextFloat();
+    }
+    public void showDetails()
+    {
+        System.out.println("Employee ID    :"+eid);
+        System.out.println("Employee Name  :"+name);
+        System.out.println("Employee Salary:"+salary+" per month");
+    }
+}
+public class EmployeeDetails {
+    public static void main(String[] args) {
+        Employee123 e1=new Employee123();
+        e1.inputDetails();
+        e1.showDetails();
+    }
+}
